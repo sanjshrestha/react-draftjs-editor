45687ef7eb70bcf75363abc3f6a5819c88cbdc6f
sanjshrestha <sanjay.shrestha@fusemachines.com>
2023-01-24 18:25:02 +0545

    added toolbar button according to design

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   const [convertedContent, setConvertedContent] = useState(null);
 
   useEffect(() => {
-    
+
     let html = convertToHTML(editorState.getCurrentContent());
     setConvertedContent(html);
   }, [editorState]);
@@ -26,66 +26,66 @@ function App() {
     }
   }
   useEffect(() => {
-    setEditorState( EditorState.createWithContent(
+    setEditorState(EditorState.createWithContent(
       ContentState.createFromBlockArray(
         convertFromHTML('<div><h4><span>Presenting Complaints</span></h4><ul><li>Fever since 5 days</li></ul><br/><h4><span>History of Presenting Complaints</span></h4><ul><li>On sat 29/7/9 pt cut his knee on glass while attending october fest in germany</li><li>pt attended to hisown woundy keeping it clean returned to UK no significant problems</li><li>During the next week the pt’s wounded and experienced reduced movement of his knee.</li><li>He was admitted to oKG on 3/10/07 for surgical debndement of his knew wound</li><li>On fri 5/10/7 the pt began to feel unwell with fever lasting few hours and experienced sore throat</li></ul><h4><span>Past Medical History</span></h4></br><h4><span>Allergies</span></h4></br><h4><span>Drug History</span></h4></br><h4><span>Family History</span></h4></br><h4><span>Social History / Lifestyle</span></h4></br></div>'))));
   }, []);
-  
+
 
   return (
     <div className="App">
-     <div className="d-flex justify-content-center" style ={{width: 800}}> 
-     <h1>Tool Bar</h1>
-     <label>History of Presenting Complaints</label>
-      <Editor
-      //  toolbarOnFocus
-        editorState={editorState}
-        onEditorStateChange={setEditorState}
-        wrapperClassName="wrapper-class"
-        editorClassName="editor-class"
-        toolbarClassName="toolbar-class"
-        toolbarStyle={{
-          position: 'fixed',
-          top: 0,
-          width:"100%",
-          zIndex: 1000,
-          
-        }}
-        mention={{
-          separator: ' ',
-          trigger: '@',
-          suggestions: [
-            { text: 'APPLE', value: 'apple', url: 'https://www.facebook.com' },
-            { text: 'BANANA', value: 'banana', url: 'google.com' },
-            { text: 'CHERRY', value: 'cherry', url: 'cherry' },
-            { text: 'DURIAN', value: 'durian', url: 'durian' },
-            { text: 'EGGFRUIT', value: 'eggfruit', url: 'eggfruit' },
-            { text: 'FIG', value: 'fig', url: 'fig' },
-            { text: 'GRAPEFRUIT', value: 'grapefruit', url: 'grapefruit' },
-            { text: 'HONEYDEW', value: 'honeydew', url: 'honeydew' },
-          ],
-        }}
-        editorStyle={{
-          height:550,
-          minHeight: 150,
-          paddingLeft: 10,
-          paddingRight: 10,
-        }}
-        toolbar = {{
-          
-            options: ['inline', 'list',  'link', 'image',],
+      <div className="d-flex justify-content-center" style={{ width: 800 }}>
+        <h1>Tool Bar</h1>
+        <label>History of Presenting Complaints</label>
+        <Editor
+          //  toolbarOnFocus
+          editorState={editorState}
+          onEditorStateChange={setEditorState}
+          wrapperClassName="wrapper-class"
+          editorClassName="editor-class"
+          toolbarClassName="toolbar-class"
+          toolbarStyle={{
+            position: 'fixed',
+            top: 0,
+            width: "100%",
+            zIndex: 1000,
+
+          }}
+          mention={{
+            separator: ' ',
+            trigger: '@',
+            suggestions: [
+              { text: 'APPLE', value: 'apple', url: 'https://www.facebook.com' },
+              { text: 'BANANA', value: 'banana', url: 'google.com' },
+              { text: 'CHERRY', value: 'cherry', url: 'cherry' },
+              { text: 'DURIAN', value: 'durian', url: 'durian' },
+              { text: 'EGGFRUIT', value: 'eggfruit', url: 'eggfruit' },
+              { text: 'FIG', value: 'fig', url: 'fig' },
+              { text: 'GRAPEFRUIT', value: 'grapefruit', url: 'grapefruit' },
+              { text: 'HONEYDEW', value: 'honeydew', url: 'honeydew' },
+            ],
+          }}
+          editorStyle={{
+            height: 550,
+            minHeight: 150,
+            paddingLeft: 10,
+            paddingRight: 10,
+          }}
+          toolbar={{
+
+            options: ['inline', 'list', 'textAlign', 'link', 'image', 'history'],
             inline: {
               inDropdown: false,
               className: undefined,
               component: undefined,
               dropdownClassName: undefined,
-              options: ['bold'],
-              bold: {  className: undefined },
-              
+              options: ['bold', 'italic', 'underline'],
+              bold: { className: undefined },
+
             },
-           
-           
-            
+
+
+
             list: {
               inDropdown: false,
               className: undefined,
@@ -93,14 +93,22 @@ function App() {
               dropdownClassName: undefined,
               options: ['unordered', 'ordered'],
               unordered: { className: undefined },
-              ordered: {  className: undefined },
-              indent: {  className: undefined },
-              outdent: {  className: undefined },
+              ordered: { className: undefined },
+              indent: { className: undefined },
+              outdent: { className: undefined },
             },
-           
-           
+
+            textAlign: {
+              inDropdown: true,
+              className: undefined,
+              component: undefined,
+              dropdownClassName: undefined,
+              options: ['left', 'center', 'right', 'justify'],
+
+            },
+
             link: {
-             
+
               inDropdown: false,
               className: undefined,
               component: undefined,
@@ -109,14 +117,14 @@ function App() {
               showOpenOptionOnHover: true,
               defaultTargetOption: '_self',
               options: ['link'],
-              link: {  icon:Link, className: undefined },
-              
+              link: { icon: Link, className: undefined },
+
               linkCallback: undefined
             },
-            
-            
+
+
             image: {
-             icon:Image,
+              icon: Image,
               className: undefined,
               component: undefined,
               popupClassName: undefined,
@@ -132,13 +140,13 @@ function App() {
                 width: 'auto',
               },
             },
-           
-          
-        }}
-       
+
+
+          }}
+
         // toolbarHidden
-      />
-       {/* <Editor
+        />
+        {/* <Editor
       //  toolbarOnFocus
         editorState={editorState}
         onEditorStateChange={setEditorState}
